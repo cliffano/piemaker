53eaa65bc353f4b26b31f528577759896b1cd116
Cliffano Subagio <cliffano@gmail.com>
2026-09-14 14:34:10 +1000

    Fix conflog singular config file in example.

--- examples/python-lib/examples/_piemakerlibexample.py
+++ examples/python-lib/examples/_piemakerlibexample.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code,too-many-locals
 from piemakerlibexample import Display
 
-display = Display(conf_files=['piemakerlibexample.yaml'])
+display = Display(conf_file='piemakerlibexample.yaml')
 text = display.format(reverse=False, transformation='lower')
 print(text)
